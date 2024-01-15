7e95c1ef405f97b41965f782bc39a996079588b4
mduffaud-ensae <martin.duffaud@ensae.fr>
2024-01-15 10:05:31 +0000

    question 3

--- swap_puzzle/solver.py
+++ swap_puzzle/solver.py
@@ -1,6 +1,6 @@
 from grid import Grid
 
-class Solver(): 
+class Solver(Grid): 
     """
     A solver class, to be implemented.
     """
@@ -10,7 +10,22 @@ class Solver():
         Solves the grid and returns the sequence of swaps at the format 
         [((i1, j1), (i2, j2)), ((i1', j1'), (i2', j2')), ...]. 
         """
-        # TODO: implement this function (and remove the line "raise NotImplementedError").
-        # NOTE: you can add other methods and subclasses as much as necessary. The only thing imposed is the format of the solution returned.
-        raise NotImplementedError
+        n = self.n
+        m = self.m
+        while not self.is_sorted :
+            for i in range (m):
+                for j in range (n):
+                    if j < n-1 :
+                        cell1 = (i, j)
+                        cell2 = (i, j+1)
+                        if self.state[i][j] > self.state[i][j+1]:
+                            self.swap(cell1,cell2)
+                    if i < m-1 :
+                        cell1 = (i, j)
+                        cell2 = (i+1, j)
+                        if self.state[i][j] > self.state[i+1][j]:
+                            self.swap(cell1, cell2)
+        
+
+
 
