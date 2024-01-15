94aa1e848d4f9dec3917708772aecec8922d993f
mduffaud-ensae <martin.duffaud@ensae.fr>
2024-01-15 10:43:13 +0000

    test get solution finished

---
+++ tests/test_get_solution.py
@@ -0,0 +1,17 @@
+# This will work if ran from the root folder ensae-prog24
+import sys 
+sys.path.append("../swap_puzzle/")
+
+import unittest 
+from grid import Grid
+from solver import Solver
+
+class Test_get_solution(unittest.TestCase):
+    def test_grid1(self):
+        g = Grid.grid_from_file("../input/grid1.in")
+        s = Solver()
+        s.get_solution(g)
+        self.assertEqual(g.is_sorted(), True)
+
+if __name__ == '__main__':
+    unittest.main()
